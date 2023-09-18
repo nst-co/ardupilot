d25255fa71e0532fe112b87ca9ac98dd760b32a9
hatakeyama <hatakeyama@nst-co.com>
2023-09-18 11:59:39 +0900

    NST UFO fix

--- libraries/AR_Motors/AP_MotorsUGV.cpp
+++ libraries/AR_Motors/AP_MotorsUGV.cpp
@@ -273,8 +273,8 @@ float AP_MotorsUGV::get_slew_limited_throttle(float throttle, float dt) const
  */
 bool AP_MotorsUGV::have_skid_steering() const
 {
-    if (SRV_Channels::function_assigned(SRV_Channel::k_throttleLeft) &&
-        SRV_Channels::function_assigned(SRV_Channel::k_throttleRight || is_omni())) {
+    if ((SRV_Channels::function_assigned(SRV_Channel::k_throttleLeft) &&
+        SRV_Channels::function_assigned(SRV_Channel::k_throttleRight)) || is_omni()) {
         if (SRV_Channels::function_assigned(SRV_Channel::k_steering)) {
             // regular + drive force distribution
             return false;
