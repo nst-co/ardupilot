a8bf03b5599d11465139a125322031b9878a251d
hatakeyama <hatakeyama@nst-co.com>
2025-06-23 08:39:34 +0900

    fix gps baudrate

--- libraries/AP_GPS/AP_GPS.cpp
+++ libraries/AP_GPS/AP_GPS.cpp
@@ -764,10 +764,10 @@ AP_GPS_Backend *AP_GPS::_detect_instance(const uint8_t instance)
             return NEW_NOTHROW AP_GPS_UBLOX(*this, params[instance], state[instance], port, GPS_ROLE_NORMAL);
         }
 
-        const uint32_t ublox_mb_required_baud = option_set(DriverOptions::UBX_MBUseUart2)?230400:460800;
+        //const uint32_t ublox_mb_required_baud = option_set(DriverOptions::UBX_MBUseUart2)?230400:460800;
         if ((type == GPS_TYPE_UBLOX_RTK_BASE ||
              type == GPS_TYPE_UBLOX_RTK_ROVER) &&
-            _baudrates[dstate->current_baud] == ublox_mb_required_baud &&
+            /* _baudrates[dstate->current_baud] == ublox_mb_required_baud && */
             AP_GPS_UBLOX::_detect(dstate->ublox_detect_state, data)) {
             GPS_Role role;
             if (type == GPS_TYPE_UBLOX_RTK_BASE) {
